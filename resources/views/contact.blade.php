03d6897ee7e1e7db9204d2d9aa8ae10613b6456c
unknown <anthony.cerdas.chacon@est.una.ac.cr>
2026-03-17 10:17:52 -0600

    ajustes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>aplicacion-2</title>
+    @livewireStyles
 </head>
 <body>
     <h1>Formulario de Contacto</h1>
 
     <livewire:contact-form />
 
+    @livewireScripts
 </body>
 </html>
