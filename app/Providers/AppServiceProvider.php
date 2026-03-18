a168bc902e67fb1d81521b4ff37acb496d6367fe
José Arrieta Sancho <jose.arrieta.sancho@est.una.ac.cr>
2026-03-17 19:31:50 -0600

    fiX

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('production')) {
-            URL::forceScheme('https');
+        URL::forceRootUrl(config('app.url'));
+        URL::forceScheme('https');
         }
     }
 }
